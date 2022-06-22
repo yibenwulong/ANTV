d94046df5b90f9d6292c986973a219821ab8d4f3
liyuner <2624164589@qq.com>
2022-06-22 22:42:05 +0800

    add

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,28 @@
 // Dart与HTML集成 （https://dart.dev/tutorials/web/low-level-html/connect-dart-html）
 
 import 'dart:html';
+import 'dart:js';
 
-void main() {
-  int a = 1;
-  int b = 2;
-  int c = a + b;
-  querySelector('#RipVanWinkle')!.innerHtml =
-      '诶，如果看到这句话，说明Dart已经集成到了HTML中了！<br/> $c = $a + $b';
+void main(List<String> args) {
+  // 取得用于显示dart是否连接成功的div
+  final div = querySelector("#hint") as DivElement;
+  div.text = "dart connected.";
+
+  // 从js上下文中获取chart类
+  var chart = context['chart'];
+  // 构件js型的变量data
+  var data = JsObject.jsify([
+    {'type': '教育', 'value': '20'},
+    {'type': '餐饮', 'value': '30'},
+    {'type': '医疗', 'value': '10'},
+    {'type': '交通', 'value': '10'},
+    {'type': '娱乐', 'value': '20'},
+    {'type': '其他', 'value': '10'}
+  ]);
+
+  // 调用chart的source与render函数向其传入新的数据并使其重新渲染
+  chart.callMethod('source', [data]);
+  chart.callMethod('render', []);
+
+  print('data updated');
 }
